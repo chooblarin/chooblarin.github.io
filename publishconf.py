67348f9f8ac90a4937a32e9b9aca33308f498ee8
chooblarin <choo.bla.rin@gmail.com>
2015-06-13 13:10:24 +0900

    Organize..

--- publishconf.py
+++ publishconf.py
@@ -14,6 +14,7 @@ SITEURL = 'http://chooblarin.github.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
+FEED_ALL_RSS = 'feeds/all.rss.xml'
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
