5bfac193b868029b443ad845d216aa37245a09f2
Sota Hatakeyama <choo.bla.rin@gmail.com>
2018-12-02 18:50:59 +0900

    Add the count of post for each tags

--- src/templates/tag-list.js
+++ src/templates/tag-list.js
@@ -4,7 +4,7 @@ import Link from "gatsby-link";
 export default ({ pageContext }) => {
   const { posts } = pageContext;
   const sortedPostArray = Object.keys(posts)
-    .map(key => [key, posts[key].tag])
+    .map(key => [key, posts[key].tag, posts[key].items])
     .sort((a, b) => {
       if (a[1] < b[1]) {
         return -1;
@@ -18,9 +18,11 @@ export default ({ pageContext }) => {
     <div>
       <h1>Tags</h1>
       <ul>
-        {sortedPostArray.map(([key, tag]) => (
+        {sortedPostArray.map(([key, tag, items]) => (
           <li key={key}>
-            <Link to={`/tags/${key}`}>{tag}</Link>
+            <Link to={`/tags/${key}`}>
+              {tag} ({items.length})
+            </Link>
           </li>
         ))}
       </ul>
