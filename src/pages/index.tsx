63ec942984a73523de485de8b9fc202e2125bb60
Sota Hatakeyama <choo.bla.rin@gmail.com>
2020-11-07 22:29:39 +0900

    Use TypeScript

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,13 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div >
+    <div>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       Hello
     </div>
-  )
+  );
 }
