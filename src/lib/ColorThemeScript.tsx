105c043cae7856ef2e5ce7bb3cb0e654286fc625
Sota Hatakeyama <choo.bla.rin@gmail.com>
2020-11-30 16:16:15 +0900

    Add ColorThemeScript

---
+++ src/lib/ColorThemeScript.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { themeColors } from "src/constants";
+
+export const ColorThemeScript: React.FC = () => (
+  <script
+    dangerouslySetInnerHTML={{
+      __html: `
+      (function() {
+        var preferDarkQuery = '(prefers-color-scheme: dark)';
+        var mql = window.matchMedia(preferDarkQuery);
+        var supportsColorSchemeQuery = mql.media === preferDarkQuery;
+
+        function getInitialColorMode() {
+          if (supportsColorSchemeQuery) {
+            return mql.matches ? 'dark' : 'light';
+          }
+          return 'light';
+        }
+
+        function setThemeColors(mode) {
+          var root = document.documentElement;
+          root.style.setProperty(
+            '--color-text',
+            mode === 'light'
+              ? '${themeColors.light.textColor}'
+              : '${themeColors.dark.textColor}'
+          );
+          root.style.setProperty(
+            '--color-background',
+            mode === 'light'
+              ? '${themeColors.light.backgroundColor}'
+              : '${themeColors.dark.backgroundColor}'
+          );
+          root.style.setProperty(
+            '--color-primary',
+            mode === 'light'
+              ? '${themeColors.light.primaryColor}'
+              : '${themeColors.dark.primaryColor}'
+          );
+        }
+
+        var colorMode = getInitialColorMode();
+        setThemeColors(colorMode);
+
+        if (supportsColorSchemeQuery) {
+          mql.addListener(function (ev) {
+            setThemeColors(ev.matches ? 'dark' : 'light');
+          });
+        }
+      })();
+    `,
+    }}
+  />
+);
