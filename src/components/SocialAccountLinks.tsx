7926af36df5174853de4fc4a267acae754ed770b
Sota Hatakeyama <choo.bla.rin@gmail.com>
2020-12-05 23:04:44 +0900

    Add CodePen link

--- src/components/SocialAccountLinks.tsx
+++ src/components/SocialAccountLinks.tsx
@@ -1,15 +1,17 @@
 import { css } from "@emotion/core";
 import React from "react";
 import { Svg } from "react-optimized-image";
+import Codepen from "src/assets/codepen.svg";
 import Email from "src/assets/email.svg";
 import Github from "src/assets/github.svg";
 import Linkedin from "src/assets/linkedin.svg";
 import Twitter from "src/assets/twitter.svg";
 
-const mailToLink = "mailto:choo.bla.rin@gmail.com";
 const twitterLink = "https://twitter.com/chooblarin";
 const githubLink = "https://github.com/chooblarin";
+const codepenLink = "https://codepen.io/chooblarin";
 const linkedInLink = "https://linkedin.com/in/sota-hatakeyama-35bb07120";
+const mailToLink = "mailto:choo.bla.rin@gmail.com";
 
 const serviceIconStyle = css`
   width: 32px;
@@ -26,25 +28,48 @@ const serviceIconStyle = css`
 export const SocialAccountLinks: React.FC = () => (
   <div
     css={css`
-      display: grid;
-      grid-auto-flow: column;
+      display: flex;
       justify-content: center;
       color: #777777;
 
-      & a {
-        padding: 8px;
+      a {
+        padding: 0 8px;
       }
     `}
   >
-    <a href={twitterLink} target="_blank" rel="noopener noreferrer">
+    <a
+      href={twitterLink}
+      title="Twitter"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
       <Svg src={Twitter} css={serviceIconStyle} />
     </a>
 
-    <a href={githubLink} target="_blank" rel="noopener noreferrer">
+    <a
+      href={githubLink}
+      title="GitHub"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
       <Svg src={Github} css={serviceIconStyle} />
     </a>
 
-    <a href={linkedInLink} target="_blank" rel="noopener noreferrer">
+    <a
+      href={codepenLink}
+      title="CodePen"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <Svg src={Codepen} css={serviceIconStyle} />
+    </a>
+
+    <a
+      href={linkedInLink}
+      title="LinkedIn"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
       <Svg src={Linkedin} css={serviceIconStyle} />
     </a>
 
