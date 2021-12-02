a6baebd0250787c6082b9ca2e900e9848691346e
Sota Hatakeyama <choo.bla.rin@gmail.com>
2021-12-02 10:59:16 +0900

    Make gtag function optional

--- src/lib/gtag.ts
+++ src/lib/gtag.ts
@@ -1,9 +1,12 @@
 export const gaMeasurementId = "UA-62520326-2";
 
+declare let window: Window & {
+  gtag?: any;
+};
+
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageView = (url: string) => {
-  const gtag = (window as any).gtag;
-  gtag("config", gaMeasurementId, {
+  window?.gtag("config", gaMeasurementId, {
     page_path: url,
   });
 };
@@ -15,8 +18,7 @@ export const event = (
   label: string,
   value: string
 ) => {
-  const gtag = (window as any).gtag;
-  gtag("event", action, {
+  window?.gtag("event", action, {
     event_category: category,
     event_label: label,
     value: value,
