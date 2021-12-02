97d78ad6c8796d0ebf09d7876d43fbb32f5ce871
Sota Hatakeyama <choo.bla.rin@gmail.com>
2021-12-02 11:22:23 +0900

    Also disable 'analytics_storage'

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -19,6 +19,7 @@ export default class CustomDocument extends Document {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('consent', 'default', {
+              'analytics_storage': 'denied',
               'ad_storage': 'denied'
             });
             gtag('set', 'ads_data_redaction', true);
