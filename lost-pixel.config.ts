94e8474b0c1e10532bf9127d0a6c0c0e1cee6c53
Sota Hatakeyama <choo.bla.rin@gmail.com>
2022-11-28 02:16:08 +0900

    Hello Lost Pixel

---
+++ lost-pixel.config.ts
@@ -0,0 +1,10 @@
+import type { CustomProjectConfig } from "lost-pixel";
+
+export const config: CustomProjectConfig = {
+  pageShots: {
+    pages: [{ path: "/", name: "top" }],
+    // IP should be localhost when running locally & 172.17.0.1 when running in GitHub action
+    baseUrl: "http://172.17.0.1:3000",
+  },
+  generateOnly: true,
+};
