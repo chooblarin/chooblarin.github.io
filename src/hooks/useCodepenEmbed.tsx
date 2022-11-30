b7a88ee684f5773feb322094c6d9ef32affdcc2c
Sota Hatakeyama <choo.bla.rin@gmail.com>
2022-12-01 03:01:22 +0900

    Update codepen js

--- src/hooks/useCodepenEmbed.tsx
+++ src/hooks/useCodepenEmbed.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export function useCodepenEmbed() {
   React.useEffect(() => {
     const script = document.createElement("script");
-    script.src = "https://static.codepen.io/assets/embed/ei.js";
+    script.src = "https://cpwebassets.codepen.io/assets/embed/ei.js";
     document.body.appendChild(script);
     return () => {
       document.body.removeChild(script);
