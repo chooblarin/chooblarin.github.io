b382c17d810f8af56421c543606b1a851319c842
Sota Hatakeyama <choo.bla.rin@gmail.com>
2023-11-29 18:08:21 +0900

    Upgrade Storybook to v7

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,14 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
-}
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+
+  docs: {
+    autodocs: true,
+  },
+};
