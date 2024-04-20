372cc418521973a76f77a88e0bda21d156aa4ff7
Sota Hatakeyama <choo.bla.rin@gmail.com>
2024-04-20 18:57:43 +0900

    Add description and image to collection data schema as optional

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,6 +4,8 @@ const post = defineCollection({
   type: "content",
   schema: z.object({
     title: z.string(),
+    description: z.string().optional(),
+    image: z.string().optional(),
     date: z.coerce.date(),
     tags: z.array(z.string()),
     draft: z.boolean().optional(),
