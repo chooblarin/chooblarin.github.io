20a0add044ff45cd2f4d621e0fe51f8fe4aa3891
Sota Hatakeyama <choo.bla.rin@gmail.com>
2024-04-20 15:52:49 +0900

    Revert "npx astro add partytown"
    This reverts commit 8513ce7c8f288b1df3d9ae309741b8d28cb02925.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,16 @@
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
-import partytown from "@astrojs/partytown";
-
 // https://astro.build/config
 export default defineConfig({
   redirects: {
-    "/page": "/page/1"
+    "/page": "/page/1",
   },
   site: "https://chooblarin.com",
-  integrations: [sitemap(), partytown()],
+  integrations: [sitemap()],
   markdown: {
     shikiConfig: {
-      theme: "night-owl"
-    }
-  }
+      theme: "night-owl",
+    },
+  },
 });
