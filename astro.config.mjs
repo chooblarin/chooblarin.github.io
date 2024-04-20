8513ce7c8f288b1df3d9ae309741b8d28cb02925
Sota Hatakeyama <choo.bla.rin@gmail.com>
2024-04-20 15:41:59 +0900

    npx astro add partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,18 @@
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
   redirects: {
-    "/page": "/page/1",
+    "/page": "/page/1"
   },
   site: "https://chooblarin.com",
-  integrations: [sitemap()],
+  integrations: [sitemap(), partytown()],
   markdown: {
     shikiConfig: {
-      theme: "night-owl",
-    },
-  },
+      theme: "night-owl"
+    }
+  }
 });
